a88b113a2525f2e52585b56147af689b4c83b614
Harshit-Verma6655 <144591839+Harshit-Verma6655@users.noreply.github.com>
2024-09-14 17:25:42 +0530

    Update News.jsx

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -18,16 +18,16 @@ function News() {
         <>
             <Header />
             {obj ? <div className='bg-gray-100'>
-                <div className='w-full  flex justify-center'>
-                    <div className='flex flex-col p-5 bg-white items-center w-[55%]'>
+                <div className='sm:w-full  sm:flex sm:justify-center'>
+                    <div className='flex text-wrap flex-col p-5 bg-white items-center w-full sm:w-[55%]'>
                         <h1 className='text-4xl mt-8 mb-4 font-black'>{obj.title}</h1>
                         <img src={obj.image} />
                         <h2 className='text-3xl my-4 text-gray-600'>{obj.description}</h2>
 
 
-                        <p className='text-lg  mb-4font-serif'>{obj.content}</p>
-                        <h2 className='my-3 font-semibold text-gray-700'><a href={obj.url}>See full article:-{obj.url}</a></h2>
-                        <div className='flex bg-white w-full flex-col items-start font-semibold text-gray-600 py-5'>
+                        <p className='sm:text-lg  mb-4font-serif'>{obj.content}</p>
+                        <h2 className='my-3  font-semibold w-full text-wrap  text-gray-700'><a href={obj.url}>See full article:-{obj.url}</a></h2>
+                        <div className='flex bg-white sm:w-full flex-col items-start font-semibold text-gray-600 py-5'>
                             <div>Source-</div>
                             <p>name: Yahoo Canada Finance</p>
                             <p>url: https://ca.finance.yahoo.com</p>
